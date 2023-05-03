4ea18150f6128411a4d5c42436f44213bbe1449c
MatheusSandes <matheus_sandes@hotmail.com>
2023-05-03 15:20:49 -0300

    feat: edit almost all the examples

--- pythonic/tests/str__repr.py
+++ pythonic/tests/str__repr.py
@@ -1,11 +1,10 @@
 class Car:
-    def __init__(self, color, mileage):
+    def __init__(self, color):
         self.color = color
-        self.mileage = mileage
 
     def __str__(self):
-        return 'a {self.color} car'.format(self=self)
+        return 'a {self.color} car!!'.format(self=self)
 
     def __repr__(self):
-        rep = 'Person(' + self.color + ',' + str(self.color) + ')'
+        rep = 'Person (' + self.color + ',' + str(self.color) + ')'
         return rep
--- pythonic/tests/total_ordering.py
+++ pythonic/tests/total_ordering.py
@@ -1,6 +1,5 @@
 from functools import total_ordering
-  
-  
+
 @total_ordering
 class Students:
     def __init__(self, cgpa):
@@ -9,25 +8,8 @@ class Students:
     def __lt__(self, other):
         return self.cgpa<other.cgpa
   
-    def __eq__(self, other):
-        return self.cgpa == other.cgpa
-  
-    def __le__(self, other):
-        return self.cgpa<= other.cgpa
-      
-    def __ge__(self, other):
-        return self.cgpa>= other.cgpa
-          
-    def __ne__(self, other):
-        return self.cgpa != other.cgpa
-  
 Arjun = Students(8.6)
   
 Ram = Students(7.5)
   
 print(Arjun.__lt__(Ram))
-print(Arjun.__le__(Ram))
-print(Arjun.__gt__(Ram))
-print(Arjun.__ge__(Ram))
-print(Arjun.__eq__(Ram))
-print(Arjun.__ne__(Ram))
